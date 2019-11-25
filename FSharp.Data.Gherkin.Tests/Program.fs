6ab1b4cb0c832e9e11989825eb5d0cab8ff28edc
Chris Roff <roffster@gmail.com>
2019-11-25 23:14:34 +0000

    test refactor

--- FSharp.Data.Gherkin.Tests/Program.fs
+++ FSharp.Data.Gherkin.Tests/Program.fs
@@ -6,24 +6,24 @@ open Expecto
 type TestFeature = FSharp.Data.Gherkin.GherkinProvider<"./test.feature">
 let feature = TestFeature.``this is a feature``
 
-[<Tests>]
-let exploratory =
-    testCase
-        "test some of the things"
-        <| fun _ ->
+// [<Tests>]
+// let exploratory =
+//     testCase
+//         "test some of the things"
+//         <| fun _ ->
             
-            let feature = TestFeature.``this is a feature``.FeatureName
+//             let feature = TestFeature.``this is a feature``.FeatureName
 
-            let scenario = TestFeature.``this is a feature``.Scenarios.``this is a scenario``.ScenarioName
-            let step = TestFeature.``this is a feature``.Scenarios.``this is a scenario``.``0. Given this is a scenario given1``.StepName
-            let scenarioOutline =TestFeature.``this is a feature``.ScenarioOutlines.``this is a scenario outline``.ScenarioName
-            let example = (TestFeature.``this is a feature``.ScenarioOutlines.``this is another scenario outline``.Examples |> Seq.toList).[0].``col umn``.Value
-            let arg = (TestFeature.``this is a feature``.Scenarios.``this is a scenario``.``0. Given this is a scenario given1``.Argument |> Seq.toList).[0].``Data``.Value
-            let arg2 = TestFeature.``this is a feature``.Scenarios.``this is a scenario``.``2. When this is a scenario when1``.Argument.Content
+//             let scenario = TestFeature.``this is a feature``.Scenarios.``this is a scenario``.ScenarioName
+//             let step = TestFeature.``this is a feature``.Scenarios.``this is a scenario``.``0. Given this is a scenario given1``.StepName
+//             let scenarioOutline =TestFeature.``this is a feature``.ScenarioOutlines.``this is a scenario outline``.ScenarioName
+//             let example = (TestFeature.``this is a feature``.ScenarioOutlines.``this is another scenario outline``.Examples |> Seq.toList).[0].``col umn``.Value
+//             let arg = (TestFeature.``this is a feature``.Scenarios.``this is a scenario``.``0. Given this is a scenario given1``.Argument |> Seq.toList).[0].``Data``.Value
+//             let arg2 = TestFeature.``this is a feature``.Scenarios.``this is a scenario``.``2. When this is a scenario when1``.Argument.Content
 
-            let example2 = (TestFeature.``this is a feature``.ScenarioOutlines.``this is a scenario outline``.Examples |> Seq.toList).[0].qqqq.Value
+//             let example2 = (TestFeature.``this is a feature``.ScenarioOutlines.``this is a scenario outline``.Examples |> Seq.toList).[0].qqqq.Value
             
-            printfn "Feature:%A Scenario:%A Scenario Outline:%A Step:%A Example:%A DataArg:%A StringArg:%A" feature scenario scenarioOutline step example2 arg arg2
+//             printfn "Feature:%A Scenario:%A Scenario Outline:%A Step:%A Example:%A DataArg:%A StringArg:%A" feature scenario scenarioOutline step example2 arg arg2
 
 
 [<Tests>]
@@ -34,20 +34,20 @@ let scenarios =
     testList
         feature.FeatureName
         [
+            let scenario = feature.Scenarios.``a new scenario`` 
             testCase
-                scenarios.``a new scenario``.ScenarioName
+                scenario.ScenarioName
                 <| fun _ ->
-                    let scenario =scenarios.``a new scenario``
                     printfn
                         "%s\r\n%s\r\n%s" 
                         scenario.``0. Given this is a brand new step``.StepText
                         scenario.``1. When the user is emailed``.StepText
                         scenario.``2. Then something``.StepText
 
+            let scenario = scenarios.``this is a scenario``
             testCase
-                scenarios.``this is a scenario``.ScenarioName
+                scenario.ScenarioName
                 <| fun _ ->
-                    let scenario =scenarios.``this is a scenario``
                     printfn
                         "%s\r\n%s\r\n%s" 
                         scenario.``0. Given this is a scenario given1``.StepText
